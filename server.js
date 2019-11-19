a9f9b4f3a737f32d4a9dfaf887b5b5aa94af6a17
RAMON URIEL PAULO DE SANTANA <FR041969@catolica.ubec>
2019-11-19 20:00:26 -0300

    correção do server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const porta = 3000
+const porta = process.env.PORT || 3000
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
